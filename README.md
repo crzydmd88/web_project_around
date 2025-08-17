04b159a02d6f241857f8a6f0a8dfd67114bf1d7b
Victoria <crzydmd88@gmail.com>
2025-08-17 06:33:47 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Tripleten web_project_around
+
+TripleTen SPRINT 7 Around the US  --- Página web responsiva (HTML y CSS) e introducción a javascript. Se siguieron las instrucciones del brief en Figma "Alrededor de los EEUU" para la elaboración de una página interactiva en la que los usuarios pueden añadir, eliminar o dar "me gusta" a las fotos.
+
+Diseño Responsivo - Adaptación fluida para las siguientes pantallas: Desktop (1280px) Tablet (768px) Móvil (320px)
+
+Tecnologîas/técnicas del proyecto: HTML5, CSS, Javascript, Diseño Responsivo, Metodología BEM, Media Query, Importación bloques CSS y Figma.
+
+Autor: Victoria Maldonado. 
+
+Enlace:
+https://crzydmd88.github.io/web_project_around/
---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let butEdit = document.querySelector(".main__button_edit");
+let popup = document.querySelector(".popup");
+let butClose = document.querySelector(".popup__button_close");
+let form = document.querySelector(".popup__container");
+let inName = document.querySelector(".main__paragraph_name");
+let inAbout = document.querySelector(".main__paragraph_about");
+let inpName = document.querySelector(".popup__input_name");
+let inpAbout = document.querySelector(".popup__input_about");
+
+function openEdit() {
+  inpName.value = inName.textContent;
+  inpAbout.value = inAbout.textContent;
+  popup.classList.toggle("popup_opened");
+}
+
+butEdit.addEventListener("click", openEdit);
+butClose.addEventListener("click", openEdit);
+
+function saveChange(e) {
+  e.preventDefault();
+  inName.textContent = inpName.value;
+  inAbout.textContent = inpAbout.value;
+  openEdit();
+}
+
+form.addEventListener("submit", saveChange);
+
+const hearts = document.querySelectorAll(".heart");
+
+hearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    heart.classList.toggle("liked");
+    heart.textContent = heart.classList.contains("liked") ? "♥" : "♡";
+  });
+});
